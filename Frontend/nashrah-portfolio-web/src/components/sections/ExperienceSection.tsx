50a573c0b37b9e0a1813c0e5752fd5ecdda4a664
Nashrah Fatema Shaikh <nashrahfatema@gmail.com>
2026-08-23 23:59:22 +0530

    feat(react): update experience section

--- Frontend/nashrah-portfolio-web/src/components/sections/ExperienceSection.tsx
+++ Frontend/nashrah-portfolio-web/src/components/sections/ExperienceSection.tsx
@@ -4,8 +4,12 @@ import TimelineItem from "@/components/cards/TimelineItem";
 export default function ExperienceSection() {
     return (
         <section className="px-8 py-24">
-            <div className="mx-auto max-w-6xl">    
-                <h2 className="mb-12 text-4xl font-bold">
+            <div className="mx-auto max-w-4xl">
+                <p className="mb-2 text-xs font-semibold uppercase tracking-widest" style={{ color: "var(--accent)" }}>
+                My journey
+                </p>
+
+                <h2 className="mb-14 font-serif text-4xl font-bold gradient-text" >
                     Experience
                 </h2>
 
